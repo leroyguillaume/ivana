a8fa84ef673e7d43f04167164460cd5a87afb16e
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2020-04-04 21:26:42 +0200

    :green_heart: Fix CI for webapp module

--- webapp/webapp.gradle.kts
+++ webapp/webapp.gradle.kts
@@ -1,45 +1,57 @@
 import com.moowork.gradle.node.npm.NpmTask
 
 plugins {
-  id("com.github.node-gradle.node") version "2.2.0"
+    id("com.github.node-gradle.node") version "2.2.0"
 }
 
 node {
-  version = "12.16.1"
-  npmVersion = "6.13.4"
+    version = "12.16.1"
+    npmVersion = "6.13.4"
 
-  download = true
+    download = true
 }
 
 tasks {
-  getByName("npm_install") {
-    group = "build"
+    getByName("npm_install") {
+        group = "build"
 
-    outputs.upToDateWhen { projectDir.resolve("node_modules").exists() }
-  }
-
-  create<NpmTask>("assemble") {
-    group = "build"
-    dependsOn("npm_install")
-
-    setArgs(listOf("run", "build"))
-
-    outputs.upToDateWhen { buildDir.exists() }
-  }
-
-  create<NpmTask>("check") {
-    group = "verification"
-    dependsOn("assemble")
-
-    setArgs(listOf("run", "test", "--", "--no-watch", "--no-progress", "--browsers=ChromeHeadlessCI"))
-  }
-
-  create("clean") {
-    group = "build"
-
-    doLast {
-      buildDir.deleteRecursively()
-      projectDir.resolve("node_modules").deleteRecursively()
+        outputs.upToDateWhen { projectDir.resolve("node_modules").exists() }
     }
-  }
+
+    create<NpmTask>("assemble") {
+        group = "build"
+        dependsOn("npm_install")
+
+        setArgs(listOf("run", "build"))
+
+        outputs.upToDateWhen { buildDir.exists() }
+    }
+
+    create<NpmTask>("test") {
+        group = "verification"
+        dependsOn("assemble")
+
+        setArgs(listOf("run", "test", "--", "--configuration=ci"))
+    }
+
+    create<NpmTask>("e2e") {
+        group = "verification"
+        dependsOn("assemble")
+
+        setArgs(listOf("run", "e2e", "--", "--configuration=ci"))
+    }
+
+    create("check") {
+        group = "verification"
+        dependsOn("test", "e2e")
+    }
+
+    create("clean") {
+        group = "build"
+
+        doLast {
+            buildDir.deleteRecursively()
+            projectDir.resolve("node_modules").deleteRecursively()
+        }
+    }
 }
