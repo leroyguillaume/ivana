becdc7f503861b902b35d1cd4590ee96e439bd54
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2020-06-13 18:12:06 +0200

    :sparkles: add user and album name autocompletion
    Closes #51

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,7 +5,7 @@ import fr from '@angular/common/locales/fr'
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {LoginComponent} from './login/login.component'
-import {ReactiveFormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
@@ -67,7 +67,8 @@ registerLocaleData(fr)
     HttpClientModule,
     FontAwesomeModule,
     NgbModule,
-    HammerModule
+    HammerModule,
+    FormsModule
   ],
   providers: [
     {
